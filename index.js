de41ec2670d2df7235bf4186b13411cd70be0109
Lizzie LaShark <email": null>
2016-04-21 16:04:42 +1200

    tweaked old stuff

--- index.js
+++ index.js
@@ -2,40 +2,53 @@ var xhr = require('xhr')
 var example = require('./views/example.hbs')
 var $ = require('jquery')
 
-// xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-//   if (err) console.log(err) // do something
+xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+  if (err) console.log(err) // do something
 
-//   console.log(data.body)
-//   document.body.innerHTML = example({ name: "Space" });
-// // }))
-
-$(document).ready(function() {
-
-  var getMeowth = function() {
-    console.log("testing")
-    xhr.get('http://pokeapi.co/api/v2/pokemon/meowth', function(err, data) {
-    if (err) console.log(err) // do something
-      var JSONd = JSON.parse(data.body)
-      var ability = JSONd.abilities[0].ability.name
-  // console.log(JSONd)
-  // console.log(JSONd.name)
-  // console.log(JSONd.abilities[0].ability.name)
-  // console.log(JSONd.abilities[1].ability.name)
-  // console.log(JSONd.abilities[2].ability.name)
-    document.body.innerHTML = example({ name: "Pokemon", pokeName: JSONd.name,
-      ability1: JSONd.abilities[0].ability.name, ability2: JSONd.abilities[1].ability.name,
-      ability3: JSONd.abilities[2].ability.name });
-
-    })
-  }
-
-    $("button").click(getMeowth)
+  console.log(data.body)
+  document.body.innerHTML = example({ name: "Space" });
 })
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//
+//   var getMeowth = function() {
+//     console.log("testing")
+//     xhr.get('http://pokeapi.co/api/v2/pokemon/meowth', function(err, data) {
+//     if (err) console.log(err) // do something
+//       var JSONd = JSON.parse(data.body)
+//       var ability = JSONd.abilities[0].ability.name
+//   // console.log(JSONd)
+//   // console.log(JSONd.name)
+//   // console.log(JSONd.abilities[0].ability.name)
+//   // console.log(JSONd.abilities[1].ability.name)
+//   // console.log(JSONd.abilities[2].ability.name)
+//     document.body.innerHTML = example({ name: "Pokemon", pokeName: JSONd.name,
+//       ability1: JSONd.abilities[0].ability.name, ability2: JSONd.abilities[1].ability.name,
+//       ability3: JSONd.abilities[2].ability.name });
+//
+//     })
+//   }
+//
+//     $("button").click(getMeowth)
+// })
 
 
 // xhr.get('http://pokeapi.co/api/v2/ability/52', function(err, data) {
@@ -58,5 +71,3 @@ $(document).ready(function() {
 //   // console.log(JSONd.name)
 //   document.body.innerHTML = example({ name: "Pokemon!" });
 // })
-
-
